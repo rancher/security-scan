48539756d3e5801faa52b7f1f26020f852b92ab8
Murali Paluru <leodotcloud@gmail.com>
2019-11-11 06:45:33 -0800

    update kube-bench version

--- package/run.sh
+++ package/run.sh
@@ -8,52 +8,73 @@ defaultCMName=cis-$(date +"%Y-%m-%d-%H-%M-%S-%N")
 CONFIGMAPNAME=${CONFIGMAPNAME:-${defaultCMName}}
 
 # Run sonobuoy first
-sonobuoy master -v 3
-if [ $? -ne 0 ]; then
+if ! sonobuoy master -v 3
+then
   echo "error running sonobuoy"
   exit 1
 fi
 
 # This is hardcoded in the tool itself
-SONOBUOY_NS=heptio-sonobuoy
-SONOBUOY_POD_NAME=sonobuoy
+SONOBUOY_NS=${SONOBUOY_NS:-sonobuoy}
+SONOBUOY_POD_NAME=${SONOBUOY_POD_NAME:-sonobuoy}
 
 DONE_ANNOTATION_KEY="field.cattle.io/sonobuoyDone"
 DONE_ANNOTATION_VALUE="true"
 
 # Run summarizer
 SONOBUOY_OUTPUT_DIR=${SONOBUOY_OUTPUT_DIR:-/tmp/sonobuoy}
-SONOBUOY_OUTPUT_FILE=$(ls -1 ${SONOBUOY_OUTPUT_DIR}/*.tar.gz)
+SONOBUOY_OUTPUT_FILE=$(ls -1 "${SONOBUOY_OUTPUT_DIR}"/*.tar.gz)
 
 KUBE_BENCH_SUMMARIZER_ROOT=${KUBE_BENCH_SUMMARIZER_ROOT:-/tmp/kube-bench-summarizer}
 
-mkdir -p ${KUBE_BENCH_SUMMARIZER_ROOT}/{input,output}
-tar -C ${KUBE_BENCH_SUMMARIZER_ROOT}/input -xvf ${SONOBUOY_OUTPUT_FILE} --warning=no-timestamp
+mkdir -p "${KUBE_BENCH_SUMMARIZER_ROOT}"/{input,output}
+if ! tar -C "${KUBE_BENCH_SUMMARIZER_ROOT}"/input \
+         -xvf "${SONOBUOY_OUTPUT_FILE}" \
+         --warning=no-timestamp
+then
+  echo "error extracting ${SONOBUOY_OUTPUT_FILE}"
+  exit 1
+fi
 
 PLUGIN_NAME=${PLUGIN_NAME:-rancher-kube-bench}
 KBS_INPUT_DIR=${KUBE_BENCH_SUMMARIZER_ROOT}/input/plugins/${PLUGIN_NAME}/results
 KBS_OUTPUT_DIR=${KUBE_BENCH_SUMMARIZER_ROOT}/output
 KBS_OUPTPUT_FILENAME=report.json
 
-kube-bench-summarizer --failures-only --input-dir ${KBS_INPUT_DIR} --output-dir ${KBS_OUTPUT_DIR}
-if [ $? -ne 0 ]; then
+KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+K8S_API_VERSION=$(curl -sSk \
+-H "Authorization: Bearer $KUBE_TOKEN" \
+"https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/version" | jq -r '.major + "." +.minor')
+
+RANCHER_K8S_VERSION="rke-${K8S_API_VERSION}"
+echo "Rancher Kubernetes Version: ${RANCHER_K8S_VERSION}"
+
+if ! kube-bench-summarizer \
+      --k8s-version "${RANCHER_K8S_VERSION}" \
+      --input-dir "${KBS_INPUT_DIR}" \
+      --output-dir "${KBS_OUTPUT_DIR}"
+then
   echo "error running kube-bench-summarizer"
   exit 1
 fi
 
 # Create a config map with results
-kubectl -n ${SONOBUOY_NS} \
-  create cm  ${CONFIGMAPNAME} \
-  --from-file ${KBS_OUTPUT_DIR}/${KBS_OUPTPUT_FILENAME}
-if [ $? -ne 0 ]; then
+if ! kubectl -n "${SONOBUOY_NS}" \
+  create cm  "${CONFIGMAPNAME}" \
+  --from-file "${KBS_OUTPUT_DIR}"/${KBS_OUPTPUT_FILENAME}
+then
   echo "error creating configmap for storing the report"
   exit 1
 fi
 
 # Annotate self (pod) to signal "done"
-kubectl -n ${SONOBUOY_NS} \
+if ! kubectl -n "${SONOBUOY_NS}" \
   annotate pod ${SONOBUOY_POD_NAME} \
   ${DONE_ANNOTATION_KEY}=${DONE_ANNOTATION_VALUE}
+then
+  echo "error annotating self pod"
+  exit 1
+fi
 
 # Wait
 # The controller will remove this chart once the done annotation is detected
--- package/run_sonobuoy_plugin.sh
+++ package/run_sonobuoy_plugin.sh
@@ -13,9 +13,10 @@ done
 KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
 K8S_API_VERSION=$(curl -sSk \
 -H "Authorization: Bearer $KUBE_TOKEN" \
-https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/version | jq -r '.major + "." +.minor')
+"https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/version" | jq -r '.major + "." +.minor')
 
-echo "Kubernetes version: ${K8S_API_VERSION}"
+RANCHER_K8S_VERSION="rke-${K8S_API_VERSION}"
+echo "Rancher Kubernetes Version: ${RANCHER_K8S_VERSION}"
 
 #set -e
 set -x
@@ -25,7 +26,7 @@ TAR_FILE_NAME="${TAR_FILE_NAME:-kb}"
 CONFIG_DIR="${CONFIG_DIR:-/cfg}"
 RESULTS_DIR="${RESULTS_DIR:-/tmp/results}"
 
-mkdir -p ${RESULTS_DIR}
+mkdir -p "${RESULTS_DIR}"
 
 # Run kube-bench on master
 IS_MASTER=0
@@ -39,40 +40,40 @@ if [[ "$(pgrep etcd)" -gt 0 ]]; then
   MASTER_GROUPS="${MASTER_GROUPS},1.5"
 fi
 
-# Skip 1.6, no scored
+# Skip 1.6, not scored
 MASTER_GROUPS="${MASTER_GROUPS},1.7"
 
 if [[ "${IS_MASTER}" -gt 0 ]]; then
-  kube-bench master \
+  if ! kube-bench master \
     --v=5 \
-    --config-dir=${CONFIG_DIR} \
-    --group=${MASTER_GROUPS} \
-    --version ${K8S_API_VERSION} \
+    --config-dir="${CONFIG_DIR}" \
+    --group="${MASTER_GROUPS}" \
+    --version "${RANCHER_K8S_VERSION}" \
     --json \
   > "${RESULTS_DIR}/master.json"
-  if [ $? -ne 0 ]; then
+  then
     echo "error running kube-bench master"
   fi
 fi
 
 # Run kube-bench on node
 if [[ "$(pgrep kubelet)" -gt 0 ]]; then
-  kube-bench node \
+  if ! kube-bench node \
     --v=5 \
-    --config-dir=${CONFIG_DIR} \
-    --version ${K8S_API_VERSION} \
+    --config-dir="${CONFIG_DIR}" \
+    --version "${RANCHER_K8S_VERSION}" \
     --json \
   > "${RESULTS_DIR}/node.json"
-  if [ $? -ne 0 ]; then
+  then
     echo "error running kube-bench node"
   fi
 fi
 
-cd ${RESULTS_DIR}
-tar -czf ${TAR_FILE_NAME}.tar.gz *
+cd "${RESULTS_DIR}" || exit 1
+tar -czf "${TAR_FILE_NAME}.tar.gz" *
 
 if [[ "${DEBUG}" == "true" ]]; then
-    sleep ${DEBUG_TIME_IN_SEC}
+    sleep "${DEBUG_TIME_IN_SEC}"
 fi
 
 # Inform sonobuoy worker about completion of the job
