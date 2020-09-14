3ff40b2b96f5f741f339874d46e73279a4744d86
Brian Downs <brian.downs@gmail.com>
2020-09-14 13:44:22 -0700

    remove unused function
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- pkg/kb-summarizer/summarizer/summarizer.go
+++ pkg/kb-summarizer/summarizer/summarizer.go
@@ -480,25 +480,6 @@ func getGroupWrapper(group *kb.Group) *GroupWrapper {
 	}
 }
 
-
-func getMappedState(state kb.State) State {
-	switch state {
-	case kb.PASS:
-		return Pass
-	case kb.FAIL:
-		return Fail
-	case kb.WARN:
-		return Fail
-	case kb.INFO:
-		return NotApplicable
-	case SKIP:
-		return Skip
-	case NA:
-		return NotApplicable
-	}
-	return Fail
-}
-
 func getCheckWrapper(check *kb.Check) *CheckWrapper {
 	return &CheckWrapper{
 		ID:          check.ID,
