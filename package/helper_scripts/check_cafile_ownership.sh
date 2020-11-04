6ee3d1d5f8f76f969db10874bee814dcf212e63c
mrajashree <rajashree.28m@gmail.com>
2020-11-04 11:35:56 -0800

    Move node 4.1.7 and 4.1.8 tests to script to avoid errors
    On running the scan for CIS 1.6, the scan got stuck in Error state with the following error:
    ```
    level=fatal msg=\"error summarizing: \\nfailed to check if command: \\\"CAFILE=$(ps\\\" is valid exit status 2
    ```
    It looks like the command isn't getting parsed. Moving the audit commands to scripts and running the
    scripts seems to resolve this issue.

---
+++ package/helper_scripts/check_cafile_ownership.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CAFILE=$(ps -ef | grep kubelet | grep -v apiserver | grep -- --client-ca-file= | awk -F '--client-ca-file=' '{print $2}' | awk '{print $1}')
+if test -z $CAFILE; then CAFILE=$kubeletcafile; fi
+if test -e $CAFILE; then stat -c %U:%G $CAFILE; fi
