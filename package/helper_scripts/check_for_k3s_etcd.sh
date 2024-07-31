7640baad90b685f949a3c9fa767a79dcb593c341
Derek Nola <derek.nola@suse.com>
2024-07-31 10:26:55 -0700

    Fix audits and remediations for K3s 2.X checks
    - Simplify the check_for_k3s_etch.sh script
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- package/helper_scripts/check_for_k3s_etcd.sh
+++ package/helper_scripts/check_for_k3s_etcd.sh
@@ -16,38 +16,19 @@ if [[ "$(journalctl -D $JOURNAL_LOG --lines=0 2>&1 | grep -s 'No such file or di
 fi
 
 if [[ "$(journalctl -D $JOURNAL_LOG -u k3s | grep -m1 'Managed etcd cluster' | wc -l)" -gt 0 ]]; then
-    case $1 in
-        "2.1")
-            echo $(grep -A 5 'client-transport-security' /var/lib/rancher/k3s/server/db/etcd/config | grep -E 'cert-file|key-file');;
-        "2.2")
-            echo $(grep -A 5 'client-transport-security' /var/lib/rancher/k3s/server/db/etcd/config | grep 'client-cert-auth');;
-        "2.3")
-            echo $(grep 'auto-tls' /var/lib/rancher/k3s/server/db/etcd/config);;
-        "2.4")
-            echo $(grep -A 5 'peer-transport-security' /var/lib/rancher/k3s/server/db/etcd/config | grep -E 'cert-file|key-file');;
-        "2.5")
-            echo $(grep -A 5 'peer-transport-security' /var/lib/rancher/k3s/server/db/etcd/config | grep 'client-cert-auth');;
-        "2.6")
-            echo $(grep 'peer-auto-tls' /var/lib/rancher/k3s/server/db/etcd/config);;
-        "2.7")
-            echo $(grep 'trusted-ca-file' /var/lib/rancher/k3s/server/db/etcd/config);;
-    esac
+    cat /var/lib/rancher/k3s/server/db/etcd/config
 else
-# If another database is running, return whatever is required to pass the scan
-    case $1 in
-        "2.1")
-            echo "cert-file AND key-file";;
-        "2.2")
-            echo "--client-cert-auth=true";;
-        "2.3")
-            echo "false";;
-        "2.4")
-            echo "peer-cert-file AND peer-key-file";;
-        "2.5")
-            echo "--client-cert-auth=true";;
-        "2.6")
-            echo "--peer-auto-tls=false";;
-        "2.7")
-            echo "--trusted-ca-file";;
-    esac
+# If another database is running, return a fake etcd config that passes the checks
+cat <<EOF
+client-transport-security:
+    cert-file: /var/lib/rancher/k3s/server/tls/etcd/server-client.crt
+    client-cert-auth: true
+    key-file: /var/lib/rancher/k3s/server/tls/etcd/server-client.key
+    trusted-ca-file: /var/lib/rancher/k3s/server/tls/etcd/server-ca.crt
+peer-transport-security:
+    cert-file: /var/lib/rancher/k3s/server/tls/etcd/peer-server-client.crt
+    client-cert-auth: true
+    key-file: /var/lib/rancher/k3s/server/tls/etcd/peer-server-client.key
+    trusted-ca-file: /var/lib/rancher/k3s/server/tls/etcd/peer-ca.crt
+EOF
 fi
