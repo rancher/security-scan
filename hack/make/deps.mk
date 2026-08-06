1ba9abe32140b6d697746a156782fdec2afe9281
Abhishek Valaboju <abhishekvalaboju@Mac.lan>
2026-08-06 15:43:50 +0530

    bump kubectel

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -25,11 +25,11 @@ SONOBUOY_SUM_arm64 ?= ec482b5d1ec237f4c94b6fd7be5a69f95678a4331001b3edcb6ad7835e
 SONOBUOY_SUM_amd64 ?= 7c5c2250e5103c98f4dad2bcab86baf954e319c57c419c113f28f5060e62f129
 
 # renovate: datasource=github-release-attachments depName=kubernetes/kubectl
-KUBECTL_VERSION ?= 1.36.1
-# renovate: datasource=github-release-attachments depName=kubernetes/kubectl digestVersion=v1.36.1
-KUBECTL_SUM_arm64 ?= 59f7ee8e477fae658447607dc3c8790ac17a1b016c01c622c12070e969e2d4e7
-# renovate: datasource=github-release-attachments depName=kubernetes/kubectl digestVersion=v1.36.1
-KUBECTL_SUM_amd64 ?= 629d3f410e09bf49b64ae7079f7f0bda1191efed311f7d37fdbab0ad5b0ec2b7
+KUBECTL_VERSION ?= 1.36.3
+# renovate: datasource=github-release-attachments depName=kubernetes/kubectl digestVersion=v1.36.3
+KUBECTL_SUM_arm64 ?= 3d86f24401c41ae5a46ac50eef8865fe891d3647d324a0836f6c63757a126e62
+# renovate: datasource=github-release-attachments depName=kubernetes/kubectl digestVersion=v1.36.3
+KUBECTL_SUM_amd64 ?= ebbd080e7c2e275093b55915722043257eb24004363e20acb3c4d71919f88336
 
 # Reduces the code duplication on Makefile by keeping all args into a single variable.
 IMAGE_ARGS := --build-arg SONOBUOY_VERSION=$(SONOBUOY_VERSION) --build-arg SONOBUOY_SUM_arm64=$(SONOBUOY_SUM_arm64) --build-arg SONOBUOY_SUM_amd64=$(SONOBUOY_SUM_amd64) \
